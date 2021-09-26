039cdc722b1ca6c3226506aa3b574052b6c84edd
NegishiYuya <negishiyuya0115@gmail.com>
2021-09-26 22:03:43 +0900

    9/27分をアップ

---
+++ vue/1.5.1_v-if/practice.js
@@ -0,0 +1,41 @@
+var lunchMenuArray = [
+    {
+        "id": 1,
+        "name": "豚冷しゃぶともも竜田　天然ぶり刺身",
+        "price": 1000
+    },
+    {
+        "id": 2,
+        "name": "和風牛すじカレーともも竜田と和牛タタキ",
+        "price": 1100
+    },
+    {
+        "id": 3,
+        "name": "若鶏もも竜田揚げとサバ塩焼と天然ブリ刺身",
+        "price": 10000
+    }
+]
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        menuArray: lunchMenuArray,
+        isClosed: true
+    },
+    filters: {
+        parsePrice: function (price) {
+            return Number(price).toLocaleString() + '円';
+        }
+    },
+    computed: {
+        totalPrice: function () {
+            return this.menuArray.reduce(function (sum, menu) {
+                return sum + menu.price
+            }, 0)
+        },
+        totalPriceWithTax: function () {
+            return Math.floor(this.totalPrice * 1.08)
+        }
+    }
+
+});
