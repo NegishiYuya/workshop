3b57da32598ad8b72e5ae6d433a4bd964b96f9e6
NegishiYuya <negishiyuya0115@gmail.com>
2021-10-11 23:41:32 +0900

    1.7の復習問題の答え

--- vue/additional/1.7_v-bind/practice.js
+++ vue/additional/1.7_v-bind/practice.js
@@ -3,5 +3,15 @@ var app = new Vue({
     data: {
         estimate: 1000, // 予算
         expenditure: 1001 // 支出
+    },
+    filters: {
+        addSuffix: function (amount) {
+            return amount + '円'
+        }
+    },
+    computed: {
+        isOverEstimate: function () {
+            return this.estimate < this.expenditure
+        }
     }
 });
