a7d8057235f0edef673ae7e29ac237e94dccbcae
NegishiYuya <negishiyuya0115@gmail.com>
2021-11-16 23:43:53 +0900

    練習問題20を追加

---
+++ vue/additional/20/practice.js
@@ -0,0 +1,19 @@
+// 商品一覧
+var goodsArray = [
+    {
+        "id": "1", // id
+        "name": "商品1", // 商品名
+        "quantity": 0 // 数量
+
+    },
+    {
+        "id": "2",
+        "name": "商品2",
+        "quantity": 0
+    },
+    {
+        "id": "3",
+        "name": "商品3",
+        "quantity": 0
+    }
+]
