3f38cde9b702fabfcb4cc8cfcc7ac7e3a23a16e8
NegishiYuya <negishiyuya0115@gmail.com>
2021-11-21 20:37:10 +0900

    1.9.9の答え追加

--- vue/1.9.9_v-model/practice.js
+++ vue/1.9.9_v-model/practice.js
@@ -13,3 +13,35 @@ var booksArray = [
         "name": "ノラネコぐんだん ラーメンやさん"
     }
 ]
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        booksArray: booksArray,
+        radioButtonVal: "" // ラジオボタンの値
+    },
+    methods: {
+        /**
+         * 
+         * @param {Number} id 本のID
+         * @return {String} 選択中の本の名前 もしくは、「未選択です」 
+         */
+        dispBookName: function (id) {
+            // 引数のidがnullなどの場合は「未選択です」と表示する
+            if (!id) {
+                return "未選択です";
+            }
+            // 引数のidと一致する本をfindメソッドを使って抽出する
+            const dispTarget = this.booksArray.find(function (book) {
+                return book.id == id
+            })
+            return dispTarget.name
+        },
+        /**
+         * ラジオボタンの選択をクリアする
+         */
+        clearRadioButton: function () {
+            this.radioButtonVal = ""
+        }
+    }
+});
