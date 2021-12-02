a8ef6449048d889fee3f76889c0c811f7a92f2d6
NegishiYuya <negishiyuya0115@gmail.com>
2021-12-03 07:40:44 +0900

    練習問題28を追加

---
+++ vue/additional/28/practice.js
@@ -0,0 +1,26 @@
+// 登録済みの住所
+var registratedAddress = {
+    postalCode: "214-0014",
+    prefecture: 2,
+    address: "川崎市多摩区登戸"
+}
+
+// 都道府県のリスト(プルダウン用)
+var postalCodeOptionList = [
+    {
+        "value": 0,
+        "label": "選択してください"
+    },
+    {
+        "value": 1,
+        "label": "東京都"
+    },
+    {
+        "value": 2,
+        "label": "神奈川県"
+    },
+    {
+        "value": 3,
+        "label": "千葉県"
+    }
+]
