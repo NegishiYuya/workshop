1775a8b97adaefc8f98ca828bc6e4a2622f4fd55
NegishiYuya <negishiyuya0115@gmail.com>
2021-12-19 18:44:01 +0900

    練習問題30を追加

---
+++ vue/additional/30/practice.js
@@ -0,0 +1,18 @@
+// 科目選択用の配列
+var subjectOptionArray = [
+    {
+        "id": 1,
+        "value": "Japanese",
+        "label": "国語"
+    },
+    {
+        "id": 2,
+        "value": "Math",
+        "label": "数学"
+    },
+    {
+        "id": 3,
+        "value": "English",
+        "label": "英語"
+    }
+]
