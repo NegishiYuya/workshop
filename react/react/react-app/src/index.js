660329b69c55b2c846eb4caa5068577410235d98
NegishiYuya <negishiyuya0115@gmail.com>
2022-07-24 23:38:50 +0900

    7/25分作成7/25分作成7/25分作成7/25分作成7/25分作成7/25分作成7/25分作成

---
+++ react/react/react-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Reactの要素をrenderする先を定義する
+const rootElement = document.getElementById('root');
+// Reactの要素を作成する
+const root = ReactDOM.createRoot(rootElement);
+// 任意の要素を描画する
+root.render();
