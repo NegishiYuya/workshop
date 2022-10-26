6e98f411189da702d0765d1df78b41945407b99e
NegishiYuya <negishiyuya0115@gmail.com>
2022-10-26 22:46:00 +0900

    戻し

--- kotlin/study/src/workshop/07_RangeLesson.kt
+++ kotlin/study/src/workshop/07_RangeLesson.kt
@@ -5,12 +5,9 @@ package workshop
  */
 fun main(args: Array<String>) {
   // 1から10までの範囲を表すオブジェクトを作成してください
-  val intRange: IntRange = 1..10
 
   // 3が1から10までの範囲に含まれるかを判定し、結果を標準出力させてください
-  println(3 in intRange)
 
   // 1から10までの範囲オブジェクトから数字のListを作成してください
-  val listFromIntRange = intRange.toList()
-  println(listFromIntRange)
+  
 }
