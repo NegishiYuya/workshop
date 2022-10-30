565184daf376c03ddf4ca8988696a68832fceb45
NegishiYuya <negishiyuya0115@gmail.com>
2022-10-30 23:02:49 +0900

    06解答

--- kotlin/study/src/workshop/06_MapLesson.kt
+++ kotlin/study/src/workshop/06_MapLesson.kt
@@ -6,11 +6,15 @@ package workshop
 fun main(args: Array<String>) {
 
   // 読み取り専用のMapを作成してください
+  val immutableMap: Map<String, String> = mapOf("hoge" to "fuga")
 
   // 変更可のMapを作成してください
+  val mutableMap: MutableMap<String, String> = mutableMapOf("fuga" to "bar")
 
   // 変更可のMapに要素を追加してください
+  mutableMap["baz"] = "hoge"
 
   // Mapに任意のキーがあるかを判定させさせ、結果を標準出力してください
+  println(mutableMap.containsKey("baz"))
 
 }
