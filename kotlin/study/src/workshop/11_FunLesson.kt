96ea4c3040c98220ca0ac284c1d7942523adde80
NegishiYuya <negishiyuya0115@gmail.com>
2022-11-07 01:18:58 +0900

    11の解答

--- kotlin/study/src/workshop/11_FunLesson.kt
+++ kotlin/study/src/workshop/11_FunLesson.kt
@@ -8,13 +8,31 @@ fun main(args: Array<String>) {
    * (2) ラムダ式を使った実装
    */
   val nameList = listOf("太郎", "花子")
-
+  nameList.forEach(::printName)
+  nameList.forEach { println(it) }
   /**
    * 以下のようなメソッドを実装し、実行してください。
    * 第1・2引数に数字をとる、第3引数に2数を使った計算処理を返却する.
    * 戻り値はなしで、関数の処理内で第1・2引数を用いた計算結果を出力する
    */
+  calc(1, 2, ::plus)
+  calc(1, 2) { n, m -> n - m }
 }
+
+/**
+ * 引数で渡した文字列を出力する.
+ *
+ * @param name String 文字列
+ */
+fun printName(name: String) = println(name)
+
+/**
+ * 2つの数字を第3引数で渡した計算処理にて計算して結果を出力する
+ * @param x Int 1つ目の数字
+ * @param y Int 2つ目の数字
+ * @param func (Int, Int) -> Int 計算処理
+ */
+fun calc(x: Int, y: Int, func: (Int, Int) -> Int) = println(func(x, y))
 
 /**
  * 2つの数字を足して返却する
