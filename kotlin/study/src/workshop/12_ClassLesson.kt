fac364ee8ba268b0f7df16ad40500b8051ec9c79
NegishiYuya <negishiyuya0115@gmail.com>
2022-11-07 01:24:06 +0900

    12の解答

--- kotlin/study/src/workshop/12_ClassLesson.kt
+++ kotlin/study/src/workshop/12_ClassLesson.kt
@@ -5,10 +5,13 @@ fun main(args: Array<String>) {
    * クラスUserのインスタンスを生成して、メソッドを実行してください
    * プライマリコンストラクタを使用すること
    */
+  val user = User(name = "太郎", age = 32)
+  user.printUser()
 
   /**
    * クラスUserのインスタンスを生成して、メソッドを実行してください
    * セカンダリコンストラクタを使用すること
    */
-
+  val defaultUser = User(name = "次郎")
+  defaultUser.printUser()
 }
