63064296f2a3201957a0570c1b5c1331ac4be078
NegishiYuya <negishiyuya0115@gmail.com>
2022-11-20 11:32:34 +0900

    null安全についての説明を追加

---
+++ kotlin/study/src/workshop/15_NullSafeLesson.kt
@@ -0,0 +1,27 @@
+package workshop
+
+fun main(args: Array<String>) {
+
+  // 以下3つの変数の違いについて説明してください
+  val nonNullString: String = "nonNull"
+  val nullAbleString: String? = "nullAble"
+  val nullString: String? = null
+
+  // 以下の変数の値にnullAbleStringを代入してください
+  val castString: String
+
+  /**
+   * 変数nonNullString・nullAbleString・nullStringの文字列長を標準出力してください
+   * 文字列長としてnullが返却される場合については"empty"という文字列を出力すること
+   * if文を使わずに実装すること
+   */
+
+
+  /**
+   * nullAbleStringの文字列長を以下条件にしたがって、標準出力してください
+   *
+   * ・nullAbleStringがnullでないときのみ出力する
+   * ・if文を使い、セーフコール演算子は使わずに実装すること
+   */
+
+}
