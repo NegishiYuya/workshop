254517216854d82644488752b254cbe84a624fd1
NegishiYuya <negishiyuya0115@gmail.com>
2023-01-09 21:56:21 +0900

    詳細画面の製造　固定のhtmlをとりあえず表示できるようにした

--- springboot/springworkshop/src/main/kotlin/com/example/springworkshop/controller/UserController.kt
+++ springboot/springworkshop/src/main/kotlin/com/example/springworkshop/controller/UserController.kt
@@ -8,6 +8,7 @@ import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("user")
@@ -15,10 +16,15 @@ class UserController {
 
 	@Autowired
 	lateinit var userService: UserService
-
 	@GetMapping
 	fun index(model: Model): String {
 		model["userList"] = userService.getList().map { UserForm(it.id, it.name) }
 		return "list"
+	}
+
+	@GetMapping("/detail")
+	fun detail(model: Model): String {
+		model["user"] = UserForm(id = 4, name = "ホゲータ")
+		return "detail"
 	}
 }
