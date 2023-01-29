7673eb8d061cb1e1a79675c51336d071d59b5231
NegishiYuya <negishiyuya0115@gmail.com>
2023-01-29 15:31:34 +0900

    mysqlコンテナを追加

---
+++ springboot/mysql/script/init.sql
@@ -0,0 +1,13 @@
+create table user(
+    id int,
+    name varchar(20),
+    primary key (id)
+);
+
+insert into
+    user(id, name)
+values
+    (1, 'ニャオハ'),
+    (2, 'ニャローテ'),
+    (3, 'マスカーニャ'),
+    (4, 'ホゲータ');
