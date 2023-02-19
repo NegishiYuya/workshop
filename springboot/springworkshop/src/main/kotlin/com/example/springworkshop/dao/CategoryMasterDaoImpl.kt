c880113fb737e3733ad510dcc21b53e96bfb646c
NegishiYuya <negishiyuya0115@gmail.com>
2023-02-20 01:49:27 +0900

    Revert "マスタ取得処理の実装"
    This reverts commit f958c475e70ccf3bfce0a895939aa668badbc451.

--- springboot/springworkshop/src/main/kotlin/com/example/springworkshop/dao/CategoryMasterDaoImpl.kt
+++ springboot/springworkshop/src/main/kotlin/com/example/springworkshop/dao/CategoryMasterDaoImpl.kt
@@ -1,19 +1,15 @@
 package com.example.springworkshop.dao
 
 import com.example.springworkshop.dto.CategoryMasterDto
-import com.springworkshop.dbflute.exbhv.MasterCategoryBhv
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 @Repository
 class CategoryMasterDaoImpl : CategoryMasterDao {
-
-	@Autowired
-	lateinit var categoryMasterCategoryBhv: MasterCategoryBhv
-
 	override fun selectList(): List<CategoryMasterDto> {
-		return categoryMasterCategoryBhv.selectList { cb ->
-			cb.query()
-		}.map { CategoryMasterDto(id = it.id, label = it.label) }
+		return listOf(
+			CategoryMasterDto(1, "くさ"),
+			CategoryMasterDto(2, "ほのお"),
+			CategoryMasterDto(3, "みず")
+		)
 	}
 }
