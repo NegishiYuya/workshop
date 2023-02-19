a27febf1fa1c8bfcfb6f88fae3d71bf614e0e2ec
NegishiYuya <negishiyuya0115@gmail.com>
2023-02-19 18:06:33 +0900

    select処理を追加

--- springboot/springworkshop/src/main/kotlin/com/example/springworkshop/dao/UserDaoImpl.kt
+++ springboot/springworkshop/src/main/kotlin/com/example/springworkshop/dao/UserDaoImpl.kt
@@ -1,36 +1,33 @@
 package com.example.springworkshop.dao
 
 import com.example.springworkshop.entity.User
+import com.springworkshop.dbflute.exbhv.UserBhv
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 @Repository
 class UserDaoImpl : UserDao {
 
+	@Autowired
+	lateinit var userBhv: UserBhv
+
 	private val userList: List<User> = listOf(
 		User(
-			id = 1,
-			name = "ニャオハ",
-			categoryId = 1
+			id = 1, name = "ニャオハ", categoryId = 1
 		),
 		User(
-			id = 2,
-			name = "ニャローテ",
-			categoryId = 1
+			id = 2, name = "ニャローテ", categoryId = 1
 		),
 		User(
-			id = 3,
-			name = "マスカーニャ",
-			categoryId = 1
+			id = 3, name = "マスカーニャ", categoryId = 1
 		),
 		User(
-			id = 4,
-			name = "ホゲータ",
-			categoryId = 2
+			id = 4, name = "ホゲータ", categoryId = 2
 		),
 	)
 
 	override fun selectList(): List<User> {
-		return userList
+		return userBhv.selectList { cb -> cb.query() }.map { User(id = it.id, name = it.name, categoryId = it.categoryId) }
 	}
 
 	override fun select(userId: Int): User? {
