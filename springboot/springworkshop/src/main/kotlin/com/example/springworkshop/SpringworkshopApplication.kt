ac239f04f76d7a0b51d04bf95fc6714c6429be75
NegishiYuya <negishiyuya0115@gmail.com>
2023-02-19 17:58:39 +0900

    DBFluteの自動生成クラスをDIできるようにした

--- springboot/springworkshop/src/main/kotlin/com/example/springworkshop/SpringworkshopApplication.kt
+++ springboot/springworkshop/src/main/kotlin/com/example/springworkshop/SpringworkshopApplication.kt
@@ -2,8 +2,10 @@ package com.example.springworkshop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(value=["com.example.springworkshop","com.springworkshop"])
 class SpringworkshopApplication
 
 fun main(args: Array<String>) {
